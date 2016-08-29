12e0b106d5baef45752245d2fee45b0de16197ab
Sarah <sarahmichellemeyer@gmail.com>
2016-08-28 22:54:07 -0600

    players point differentials added to database

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,25 @@ router.get('/', function(req, res, next) {
   })
 });
 router.post('/create', function(req, res, next) {
-    var user1pts = req.body.undefined[0] - req.body.undefined[1];
-    var user2pts = req.body.undefined[1] - req.body.undefined[0];
-    return knex('team_game_log').insert({
-      team1_id: req.body.team1_id,
-      team2_id: req.body.team2_id,
-      team1_score: req.body.undefined[0],
-      team2_score: req.body.undefined[1]
-    }).then(function() {
-      res.redirect('/');
+  var team1_score = req.body.undefined[0];
+  var team2_score = req.body.undefined[1];
+  var team1_id = req.body.team1_id;
+  var team2_id = req.body.team2_id;
+  var user1pts = req.body.undefined[0] - req.body.undefined[1];
+  var user2pts = req.body.undefined[1] - req.body.undefined[0];
+  return knex('team_game_log').insert({
+    team1_id: team1_id,
+    team2_id: team2_id,
+    team1_score: team1_score,
+    team2_score: team2_score
+  }).then(function() {
+    if (team1_score > team2_score) {
+      return knex('team_game_stats').where('team_id', team1_id).update('pointDiff', user1pts).then(function() {
+        return knex('team_game_stats').where('team_id', team2_id).update('pointDiff', user2pts).then(function() {
+          res.redirect('/');
+        })
+      })
+    }
   })
 })
 
