b549a30c78474a164b837f8d6c5d57c317c5fbcd
Sarah <sarahmichellemeyer@gmail.com>
2016-08-31 12:36:38 -0600

    fixed bug on show page to display correct record and opponent team

--- routes/teams.js
+++ routes/teams.js
@@ -20,23 +20,32 @@ router.get('/', function(req, res, next) {
 })
 
 router.get('/:teamid', function(req, res, next) {
-  return knex('team_game_log').where('team1_id', req.params.teamid).then(function(logs) {
+  return knex('team_game_log').where('team1_id', req.params.teamid).orWhere('team2_id', req.params.teamid).then(function(logs) {
     return knex('teams').where('id', req.params.teamid).first().then(function(team) {
       Promise.all(
         logs.map(function(log) {
-          return knex('teams').where('id', log.team2_id).then(function(opponent) {
-            log.opponent = opponent;
-            return log;
-          })
-        })
-      ).then(function(logs) {
+          //if current team = team1 in log table
+          //opponent = team2 in log table - get team2 info from teams table
+          if (Number(req.params.teamid) === log.team1_id) {
+            return knex('teams').where('id', log.team2_id).then(function(opponent) {
+              log.opponent = opponent;
+              return log;
+            })
+          } else if(Number(req.params.teamid) === log.team2_id) {
+            return knex('teams').where('id', log.team1_id).then(function(opponent) {
+              log.opponent = opponent;
+              return log;
+            })
+          }
+        })).then(function(logs) {
         res.render('show', {
           logs: logs,
           team: team
         })
-      });
-    })
+      })
+    });
   })
 })
+
 
 module.exports = router;
