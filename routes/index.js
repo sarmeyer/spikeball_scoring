7085195f3ee9bbd34e15b4a514c5ebd842f0ada6
Sarah <sarahmichellemeyer@gmail.com>
2016-09-20 14:38:35 -0600

    added points for and points against to DB

--- routes/index.js
+++ routes/index.js
@@ -11,37 +11,33 @@ router.get('/', function(req, res, next) {
   })
 });
 router.post('/create', function(req, res, next) {
-      var team1_score = req.body.undefined[0];
-      var team2_score = req.body.undefined[1];
-      var team1_id = req.body.team1_id;
-      var team2_id = req.body.team2_id;
-      var user1pts = req.body.undefined[0] - req.body.undefined[1];
-      var user2pts = req.body.undefined[1] - req.body.undefined[0];
+  var team1_score = Number(req.body.undefined[0]);
+  var team2_score = Number(req.body.undefined[1]);
+  var team1_id = req.body.team1_id;
+  var team2_id = req.body.team2_id;
+  var user1pts = req.body.undefined[0] - req.body.undefined[1];
+  var user2pts = req.body.undefined[1] - req.body.undefined[0];
 
-      function stats(team1_score, team2_score, team1_id, team2_id, user1pts, user2pts) {
-        return knex('team_game_stats').where('team_id', team1_id).select('pointDiff').first().then(function(results) {
-        var pointDiff1 = results.pointDiff += user1pts;
-        return knex('team_game_stats').where('team_id', team1_id).update('pointDiff', pointDiff1).then(function() {
-              return knex('team_game_stats').where('team_id', team1_id).select('pointsFor').first().then(function(data) {
-                  var pointsFor1 = data.pointsFor += team1_score;
-                  return knex('team_game_stats').where('team_id', team1_id).update('pointsFor', pointsFor1).then(function() {
-                    return knex('team_game_stats').where('team_id', team1_id).select('pointsAgainst').first().then(function(data) {
-                      var pointsAgainst1 = data.pointsAgainst += team2_score;
-                      return knex('team_game_stats').where('team_id', team1_id).update('pointsAgainst', pointsAgainst1).then(function() {
-                        return knex('team_game_stats').where('team_id', team2_id).select('pointDiff').first().then(function(results2) {
-                          var pointDiff2 = results2.pointDiff += user2pts;
-                          return knex('team_game_stats').where('team_id', team2_id).update('pointDiff', pointDiff2).then(function() {
-                            return knex('team_game_stats').where('team_id', team2_id).select('pointsFor').first().then(function(data) {
-                              var pointsFor2 = data.pointsFor += team2_score;
-                              return knex('team_game_stats').where('team_id', team2_id).update('pointsFor', pointsFor2).then(function() {
-                                return knex('team_game_stats').where('team_id', team1_id).select('pointsAgainst').first().then(function(data) {
-                                  var pointsAgainst2 = data.pointsAgainst += team1_score;
-                                  return knex('team_game_stats').where('team_id', team2_id).update('pointsAgainst', pointsAgainst2).then(function() {
-                                    res.redirect('/');
-                                  })
-                                })
-                              })
-                            })
+  function stats(team1_score, team2_score, team1_id, team2_id, user1pts, user2pts) {
+    return knex('team_game_stats').where('team_id', team1_id).select('pointDiff').first().then(function(results) {
+      var pointDiff1 = results.pointDiff += user1pts;
+      return knex('team_game_stats').where('team_id', team1_id).update('pointDiff', pointDiff1).then(function() {
+        return knex('team_game_stats').where('team_id', team1_id).select('pointsFor').first().then(function(data) {
+          var pointsFor1 = data.pointsFor += team1_score;
+          return knex('team_game_stats').where('team_id', team1_id).update('pointsFor', pointsFor1).then(function() {
+            return knex('team_game_stats').where('team_id', team1_id).select('pointsAgainst').first().then(function(data) {
+              var pointsAgainst1 = data.pointsAgainst += team2_score;
+              return knex('team_game_stats').where('team_id', team1_id).update('pointsAgainst', pointsAgainst1).then(function() {
+                return knex('team_game_stats').where('team_id', team2_id).select('pointDiff').first().then(function(results2) {
+                  var pointDiff2 = results2.pointDiff += user2pts;
+                  return knex('team_game_stats').where('team_id', team2_id).update('pointDiff', pointDiff2).then(function() {
+                    return knex('team_game_stats').where('team_id', team2_id).select('pointsFor').first().then(function(data) {
+                      var pointsFor2 = data.pointsFor += team2_score;
+                      return knex('team_game_stats').where('team_id', team2_id).update('pointsFor', pointsFor2).then(function() {
+                        return knex('team_game_stats').where('team_id', team1_id).select('pointsAgainst').first().then(function(data) {
+                          var pointsAgainst2 = data.pointsAgainst += team1_score;
+                          return knex('team_game_stats').where('team_id', team2_id).update('pointsAgainst', pointsAgainst2).then(function() {
+                            res.redirect('/');
                           })
                         })
                       })
@@ -50,27 +46,31 @@ router.post('/create', function(req, res, next) {
                 })
               })
             })
-          }
-                return knex('team_game_log').insert({
-                  team1_id: team1_id,
-                  team2_id: team2_id,
-                  team1_score: team1_score,
-                  team2_score: team2_score
-                }).then(function() {
-                  if (team1_score > team2_score) {
-                    return knex('team_game_stats').where('team_id', team1_id).increment('wins', 1).then(function() {
-                      return knex('team_game_stats').where('team_id', team2_id).increment('losses', 1).then(function() {
-                        stats(team1_score, team2_score, team1_id, team2_id, user1pts, user2pts);
-                      })
-                    })
-                  } else {
-                    return knex('team_game_stats').where('team_id', team2_id).increment('wins', 1).then(function() {
-                      return knex('team_game_stats').where('team_id', team1_id).increment('losses', 1).then(function() {
-                        stats(team1_score, team2_score, team1_id, team2_id, user1pts, user2pts);
-                      })
-                    })
-                  }
-                })
-              })
+          })
+        })
+      })
+    })
+  }
+  return knex('team_game_log').insert({
+    team1_id: team1_id,
+    team2_id: team2_id,
+    team1_score: team1_score,
+    team2_score: team2_score
+  }).then(function() {
+    if (team1_score > team2_score) {
+      return knex('team_game_stats').where('team_id', team1_id).increment('wins', 1).then(function() {
+        return knex('team_game_stats').where('team_id', team2_id).increment('losses', 1).then(function() {
+          stats(team1_score, team2_score, team1_id, team2_id, user1pts, user2pts);
+        })
+      })
+    } else {
+      return knex('team_game_stats').where('team_id', team2_id).increment('wins', 1).then(function() {
+        return knex('team_game_stats').where('team_id', team1_id).increment('losses', 1).then(function() {
+          stats(team1_score, team2_score, team1_id, team2_id, user1pts, user2pts);
+        })
+      })
+    }
+  })
+})
 
-                  module.exports = router;
+module.exports = router;
